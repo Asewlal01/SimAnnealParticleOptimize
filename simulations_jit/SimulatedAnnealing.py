db42a9ef0f5b0c6ee9e1a15f71dd97669698e461
Asewlal <myodamian@gmail.com>
2023-12-21 22:27:56 +0100

    Now uses decreases sigma for perturbations

--- simulations_jit/SimulatedAnnealing.py
+++ simulations_jit/SimulatedAnnealing.py
@@ -1,8 +1,8 @@
 import numpy as np
-from numba import jit
+from numba import jit, prange
 
 
-@jit(nopython=True)
+@jit(nopython=True, cache=True)
 def generate_points(n, R):
     """
     Generate n random points within a circle of radius R.
@@ -26,37 +26,12 @@ def generate_points(n, R):
         y = r * np.sin(theta)
 
         # Add point to array
-        points[i] = [x,y]
+        points[i] = [x, y]
 
     return points
 
-@jit(nopython=True)
-def perturbation(i, R, current_points, step_length, current_energy, current_temp):
-    # Generate a random perturbation to change the point
-    new_points = current_points.copy()
 
-    # Get random vector
-    vec = np.random.uniform(-step_length, step_length, 2)
-
-    # Perturb
-    new_points[i] += vec
-
-    # If the new point is outside the circle, move it to the edge
-    if np.linalg.norm(new_points[i]) > R:
-        new_points[i] *= R / np.linalg.norm(new_points[i])
-
-    # Accept the new points if the new energy is lower or by a probability depending on the temperature
-    new_energy = calculate_energy(new_points)
-    energy_change = new_energy - current_energy
-    if energy_change < 0 or np.exp(
-            -energy_change / current_temp) > np.random.rand():
-        current_points = new_points
-        current_energy = new_energy
-
-    return current_points, current_energy
-
-
-@jit(nopython=True)
+@jit(nopython=True, cache=True)
 def calculate_energy(points, k=1):
     """
     Calculate the total energy of a system of charges.
@@ -78,7 +53,8 @@ def calculate_energy(points, k=1):
 
     return energy
 
-@jit(nopython=True)
+
+@jit(nopython=True, cache=True)
 def simulated_annealing(N, R, Temp_max, Temp_min, alpha, iter_num, cooling_schedule="exponential"):
     """
     Simulated annealing algorithm to minimize the energy of the system, with charges within the circle.
@@ -101,16 +77,35 @@ def simulated_annealing(N, R, Temp_max, Temp_min, alpha, iter_num, cooling_sched
 
     # Initialize array of energy history
     E = [current_energy]
-
-    step_length = R/N
-
+    
+    # Linear sigma for perturbations
+    b = np.log(N / 2) / (Temp_max - Temp_min)
+    a = R / 2 * np.exp(-b * Temp_max)
+    
     # Run simulated annealing
     while current_temp > Temp_min:
+        # Compute the sigma for perturbations
+        sigma = a * np.exp(b * current_temp)
+        
         # Run iter_num iterations at current temperature
         for _ in range(iter_num):
             # Attempt to change each point
             for i in range(len(current_points)):
-                current_points, current_energy = perturbation(i, R, current_points, step_length, current_energy, current_temp)
+                # Generate a random perturbation to change the point
+                new_points = current_points.copy()
+                new_points[i] += np.random.normal(0, sigma, size=2)
+
+                # If the new point is outside the circle, move it to the edge
+                if np.linalg.norm(new_points[i]) > R:
+                    new_points[i] *= R / np.linalg.norm(new_points[i])
+
+                # Accept the new points if the new energy is lower or by a probability depending on the temperature
+                new_energy = calculate_energy(new_points)
+                energy_change = new_energy - current_energy
+                if energy_change < 0 or np.exp(
+                        -energy_change / current_temp) > np.random.rand():
+                    current_points = new_points
+                    current_energy = new_energy
 
         # Save the energy and temperature
         E.append(current_energy)
@@ -122,6 +117,58 @@ def simulated_annealing(N, R, Temp_max, Temp_min, alpha, iter_num, cooling_sched
             current_temp -= alpha
 
     return current_points, E
+
+
+@jit(nopython=True, cache=True, parallel=True)
+def multi_simulate(N, R, Temp_max, Temp_min, alpha, iter_num, run_num):
+    """
+    Run the simulated annealing algorithm several times.
+    
+    :param N: Number of points.
+    :param R: Radius of the circle.
+    :param Temp_max: Maximum temperature.
+    :param Temp_min: Minimum temperature.
+    :param alpha: Temperature reduction factor.
+    :param iter_num: Number of iterations at each temperature.
+    :param run_num: Number of times to run the algorithm.
+    :return: Energy and positions of particles at the end of each run.
+    """
+    # Initialize arrays of energy and positions
+    E, ps = [], []
+    
+    # Run simulated annealing several times
+    for i in prange(run_num):
+        # Run simulated annealing
+        p, e = simulated_annealing(N, R, Temp_max, Temp_min, alpha, iter_num)
+        
+        # Save the energy and positions
+        E.append(e)
+        ps.append(p)
+    
+    return E, ps
+    
+def optimal_configuration(N, R, Temp_max, Temp_min, alpha, iter_num, run_num):
+    """
+    Run the simulated annealing algorithm several times to find the optimal configuration.
+
+    :param N: Number of points.
+    :param R: Radius of the circle.
+    :param Temp_max: Maximum temperature.
+    :param Temp_min: Minimum temperature.
+    :param alpha: Temperature reduction factor.
+    :param iter_num: Number of iterations at each temperature.
+    :param run_num: Number of times to run the algorithm.
+    :return: Best points and energy.
+    """
+    # Simulate annealing several times
+    E, ps = multi_simulate(N, R, Temp_max, Temp_min, alpha, iter_num, run_num)
+    
+    # Find the index of the minimum energy
+    index = np.where(E == np.min(E))[0][0]
+
+    # Return the final points and energy
+    return ps[index], E[index]
+
 
 def get_T_history(Temp_max, Temp_min, alpha):
     """
@@ -155,7 +202,6 @@ def get_E_T(N, R, Temp_max, Temp_min, alpha, iter_num, run_num):
     :param alpha: Temperature reduction factor.
     :param iter_num: Number of iterations at each temperature.
     :param run_num: Number of times to run the algorithm.
-    :param step_length: Maximum length of each perturbation.
     :return: Energy history under different temperature and temperature history.
     """
 
